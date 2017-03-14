a38c43bc3b6013a6240e97414c3a265e003ac3e9
Ron Pelt <ron.pelt@hva.nl>
2017-03-14 22:13:34 +0100

    Formulier verbetered

--- view/formulier.php
+++ view/formulier.php
@@ -7,55 +7,62 @@
         <link href="view/css/bootstrap.min.css" rel="stylesheet" media="screen">
     </head>
     <body>
-
+       
         <div class="container">
             <h2>Vul dit formulier in</h2>
             <form method="post">
                 <div class="form-group">
-                    <label for="email">email</label>
+                    <label for="fname">Email</label>
                     <input type="text" class="form-control" id="email" name="email" placeholder="Geef je voornaam">
                 </div>
                 <div class="form-group">
-                    <label for="firstName">firstName</label>
+                    <label for="lname">Voornaam</label>
                     <input type="text" class="form-control" id="firstName" name="firstName" placeholder="Geef je achternaam">
                 </div>
                 <div class="form-group">
-                    <label for="insertion">insertion</label>
+                    <label for="address">Tussenvoegsel</label>
                     <input type="text" class="form-control" id="insertion" name="insertion" placeholder="Geef je adres">
                 </div>      
                 <div class="form-group">
-                    <label for="lastName">lastName</label>
+                    <label for="zipcode">Achternaam</label>
                     <input type="text" class="form-control" id="lastName" name="lastName" placeholder="Geef je postcode">
                 </div>
                 <div class="form-group">
-                    <label for="password">password</label>
+                    <label for="city">Wachwoord</label>
                     <input type="password" class="form-control" id="password" name="password" placeholder="Geef je woonplaats">
-                </div>        
+                </div>
                 <div class="form-group">
-                    <label for="role">role</label>
-                    <input type="text" class="form-control" id="role" name="role" placeholder="Geef je woonplaats">
-                </div>        
+                    <label for="city">Wachtwoord herhalen</label>
+                    <input type="password" class="form-control" id="password2" name="password2" placeholder="Geef je woonplaats">
+                </div> 
                 <div class="form-group">
-                    <label for="schoolGroup">group</label>
-                    <input type="text" class="form-control" id="schoolGroup" name="schoolGroup" placeholder="Geef je woonplaats">
+                    <label for="city">role</label>
+                    
+                    <input type="radio" class="form-control" id="role0" name="role" value="0" checked> Leerling<br>
+                    <input type="radio" class="form-control" id="role1" name="role" value="1"> Docent<br>
+                    <input type="radio" class="form-control" id="role2" name="role" value="2"> Admin<br>
+                </div>        
+                <br>
+                <div class="form-group">
+                    <label for="city">group</label>
+                    <select class="form-control" id="schoolGroup" name="schoolGroup">
+                    <?php 
+                        $connection = new PDO('mysql:host=ronpelt.synology.me;dbname=PAD-app;charset=utf8', 'root', 'kGjMtEO06BPiu2u4');
+                        $statement = $connection->query('SELECT * FROM schoolGroup');
+
+                        while($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+                            echo '<option value="'.$row['groupName'].'">'.$row['groupName'].'</option>';
+                        }
+                    ?>
+                    </select>
                 </div>      
 
-                <button type="submit" class="btn btn-default">Submit</button>
+                <button type="submit" class="btn btn-default" >Submit</button>
             </form>
         </div>
 
-
-
-
-
-
         <script src="http://code.jquery.com/jquery.js"></script>
         <script src="view/js/bootstrap.min.js"></script>
-
-
-
-
-
 
     </body>
 </html>
