620d963cbda9044632826aacbf870ec01a57358d
Ron Pelt <ron.pelt@hva.nl>
2017-03-20 19:02:58 +0100

    vormgeef aanpassingen

--- config/config.php
+++ config/config.php
@@ -10,9 +10,3 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'kGjMtEO06BPiu2u4'); // verander dit
 define('DB_HOST', 'ronpelt.synology.me');
 define('DB_NAME', 'PAD-app');
-
-//define('DB_USERNAME', 'root');
-//define('DB_PASSWORD', 'Infinity1!'); // verander dit
-//define('DB_HOST', 'localhost');
-//define('DB_NAME', 'PAD-app');
-
