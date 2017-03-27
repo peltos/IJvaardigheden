8e1f3611f7d65daa7321b9a15a3c691f75c145fb
Ron Pelt <ron.pelt@hva.nl>
2017-03-27 14:14:26 +0200

    login google API (not finished)

--- view/login.php
+++ view/login.php
@@ -1,34 +1,86 @@
-<!DOCTYPE html>
+
+<?php
+//Include GP config file && User class
+include_once 'googleAPI/gpConfig.php';
+include_once 'googleAPI/User.php';
+
+if (isset($_GET['code'])) {
+    $gClient->authenticate($_GET['code']);
+    $_SESSION['token'] = $gClient->getAccessToken();
+    header('Location: ' . filter_var($redirectURL, FILTER_SANITIZE_URL));
+}
+
+if (isset($_SESSION['token'])) {
+    $gClient->setAccessToken($_SESSION['token']);
+}
+
+if ($gClient->getAccessToken()) {
+    //Get user profile data from google
+    $gpUserProfile = $google_oauthV2->userinfo->get();
+
+    //Initialize User class
+    $user = new User();
+
+    //Insert or update user data to the database
+    $gpUserData = array(
+        'oauth_provider' => 'google',
+        'oauth_uid' => $gpUserProfile['id'],
+        'first_name' => $gpUserProfile['given_name'],
+        'last_name' => $gpUserProfile['family_name'],
+        'email' => $gpUserProfile['email'],
+        'gender' => $gpUserProfile['gender'],
+        'locale' => $gpUserProfile['locale'],
+        'picture' => $gpUserProfile['picture'],
+        'link' => $gpUserProfile['link']
+    );
+    $userData = $user->checkUser($gpUserData);
+
+    //Storing user data into session
+    $_SESSION['userData'] = $userData;
+
+    //Render facebook profile data
+    if (strpos($userData['email'], '@ijburg') != false) {
+        if (!empty($userData)) {
+            $output = '<h1>Google+ Profile Details </h1>';
+            $output .= '<img src="' . $userData['picture'] . '" width="300" height="220">';
+            $output .= '<br/>Google ID : ' . $userData['oauth_uid'];
+            $output .= '<br/>Name : ' . $userData['first_name'] . ' ' . $userData['last_name'];
+            $output .= '<br/>Email : ' . $userData['email'];
+            $output .= '<br/>Gender : ' . $userData['gender'];
+            $output .= '<br/>Locale : ' . $userData['locale'];
+            $output .= '<br/>Logged in with : Google';
+            $output .= '<br/><a href="' . $userData['link'] . '" target="_blank">Click to Visit Google+ Page</a>';
+            $output .= '<br/>Logout from <a href="logout.php">Google</a>'; 
+        } else {
+            $output = '<h3 style="color:red">Some problem occurred, please try again.</h3>';
+        }
+    }
+    else{
+        $output = '<h3 style="color:red">You are not allowed to login. You are not part of the IJburg college</h3>';
+        $output .= '<br/>Logout from <a href="/pad/view/googleAPI/logout.php">Google</a>'; 
+        
+    }
+    
+} else {
+    $authUrl = $gClient->createAuthUrl();
+    $output = '<a href="' . filter_var($authUrl, FILTER_SANITIZE_URL) . '"><img src="images/glogin.png" alt=""/></a>';
+}
+?>
 <html>
     <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <title>Een formulier</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Bootstrap -->
         <link href="view/css/bootstrap.min.css" rel="stylesheet" media="screen">
-        
-        <meta name="google-signin-scope" content="profile email">
-        <meta name="google-signin-client_id" content="996529751909-ua9bnrd3j3bmvpei3kjj6ie89fm4ghku.apps.googleusercontent.com">
-        <script src="https://apis.google.com/js/platform.js" async defer></script>
+        <style type="text/css">
+            h1{font-family:Arial, Helvetica, sans-serif;color:#999999;}
+        </style>
     </head>
     <body>
-        <a href="admin.php"> admin </a>
-        <br>
-        
-        <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
-        <script>
-          function onSignIn(googleUser) {
-            var profile = googleUser.getBasicProfile();
-            console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-            console.log('Name: ' + profile.getName());
-            console.log('Image URL: ' + profile.getImageUrl());
-            console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present. 
-            
-            document.write('ID: ' + profile.getId());
-            document.write('<br>');
-            document.write('Name: ' + profile.getName());
-            document.write('Image URL: ' + profile.getImageUrl());
-            document.write('Email: ' + profile.getEmail());
-          }
-        </script>
+        <?php
+        print_r($userData['email'])
+        ?>
+        <div><?php echo $output; ?></div>
     </body>
 </html>
