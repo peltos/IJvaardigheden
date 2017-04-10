371949aad62b5f31b31abfd4b09bbdefd0fee884
Ron Pelt <ron.pelt@hva.nl>
2017-04-10 21:59:06 +0200

    Front-end

--- view/login.php
+++ view/login.php
@@ -34,36 +34,69 @@ if ($gClient->getAccessToken()) {
 
     //Storing user data into session
     $_SESSION['userData'] = $userData;
+    $_SESSION['userFName'] = $gpUserProfile['given_name'];
+    $_SESSION['userLName'] = $gpUserProfile['family_name'];
+    $_SESSION['userPicture'] = $gpUserProfile['picture'];
 
     //Render facebook profile data
-        if (!empty($userData)) {
-            header("Location:admin.php");
-        } else {
-            $output = '<h3 style="color:red">Some problem occurred, please try again.</h3>';
-        }
-
-    
+    if (!empty($userData)) {
+        header("Location:admin.php");
+    } else {
+        $output = '<h3 style="color:red">Some problem occurred, please try again.</h3>';
+    }
 } else {
     $authUrl = $gClient->createAuthUrl();
-    $output = '<a href="' . filter_var($authUrl, FILTER_SANITIZE_URL) . '"><img src="view/googleAPI/images/glogin.png" alt=""/></a>';
+    $output = '<a href="' . filter_var($authUrl, FILTER_SANITIZE_URL) . '"><img class="loginGoogleImage" src="view/googleAPI/images/glogin.png" alt=""/></a>';
 }
 ?>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>Een formulier</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- Bootstrap -->
-        <link href="view/css/bootstrap.min.css" rel="stylesheet" media="screen">
-        <style type="text/css">
-            h1{font-family:Arial, Helvetica, sans-serif;color:#999999;}
-        </style>
-    </head>
-    <body>
-        <?php
-        print_r($userData['email'])
-        
-        ?>
-        <div><?php echo $output; ?></div>
-    </body>
+<head>
+    <title>Login</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <!-- VENDOR CSS -->
+    <link rel="stylesheet" href="view/vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="view/vendor/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="view/vendor/linearicons/style.css">
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="view/css/main.css">
+    <link rel="stylesheet" href="view/css/custom.css">
+    <!-- GOOGLE FONTS -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
+    <!-- ICONS -->
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicon.png">
+</head>
+<body>
+    <!-- WRAPPER -->
+    <div id="wrapper">
+        <div class="vertical-align-wrap">
+            <div class="vertical-align-middle">
+                <div class="auth-box ">
+                    <div class="left">
+                        <div class="content">
+                            <?php
+                            print_r($userData['email'])
+                            ?>
+                            <div><?php echo $output; ?></div>
+                        </div>
+                    </div>
+                    <div class="right">
+                        <div class="overlay"></div>
+                        <div class="content text">
+                            <h1 class="heading"><b>IJBURG COLLEGE IJ-VAARDIGHEDEN</b></h1>
+                        </div>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END WRAPPER -->
+    <!-- Javascript -->
+    <script src="view/vendor/jquery/jquery.min.js"></script>
+    <script src="view/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="view/vendor/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+    <script src="view/js/klorofil-common.js"></script>
+</body>
 </html>
--- view/user.php
+++ view/user.php
@@ -1,82 +1,126 @@
 <?php
-//// Start the session
-//session_start();
-//if ($_SESSION['userData'] == '') {
-//    header("Location:index.php");
-//}
-//?>
+// Start the session
+session_start();
+if ($_SESSION['userData'] == '') {
+    header("Location:index.php");
+}
 
+?>
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>test</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        
-        <!-- Bootstrap -->
-        <link href="view/css/bootstrap.css" rel="stylesheet" media="screen">
-        <link href="view/css/custom.css" rel="stylesheet" media="screen">
-    </head>
-    <body>
-       
-        <div class="fluid-container">
-            <header class="col-xs-12 col-sm-4 col-md-3" style="height: 100vh; background: lightgrey; ">
-<!--                <a href="addUser.php"> add User </a>-->
-                <p>Logout from <a href="/pad/view/googleAPI/logout.php">Google</a></p>
-            </header>
-                <?php
-                    $listCount = 0;
-                    foreach($readList as $value) {
-                        echo '<a href="editUser.php?edit='.$listCount.'" class="userLink col-xs-12 col-sm-4 col-md-3">';
-                        echo '<p name="fName '.$listCount.'">'.$value['first_name'].'</p>';
-//                        echo '<p name="insertion'.$listCount.'">';
-//                        if ($value['insertion'] != null) {
-//                            echo $value['insertion'];
-//                        }
-//                        else{
-//                            echo ' - ';
-//                        }
-                        echo '<p name="lName'.$listCount.'">'.$value['last_name'].'</p>';
-                        echo '<img class="imageUser" src="'.$value['picture'].'">';
-                        
-//                        echo '</p>';
-//                        echo '<p>';
-//                        if ($value['role'] == 0) {
-//                            echo 'Leerling';
-//                        }
-//                        else if ($value['role'] == 1){
-//                            echo 'Docent';
-//                        }
-//                        else if ($value['role'] == 2){
-//                            echo 'Admin';
-//                        }
-//                        else {
-//                            echo 'Unkown error';
-//                        }
-//                        echo '</p>';
-//                        echo '<p>';
-//                        if ($value['schoolGroup'] != null) {
-//                            echo $value['schoolGroup'];
-//                        }
-//                        else{
-//                            echo ' - ';
-//                        }
-//                        echo '</p>';
-                        echo '</a>';
-                        
-                        //$_SESSION["email" . $listCount] = $value['email'];
-                        $_SESSION["fName" . $listCount] = $value['first_name'];
-                        //$_SESSION["insertion" . $listCount] = $value['insertion'];
-                        $_SESSION["lName" . $listCount] = $value['last_name'];
-                       // $_SESSION["role" . $listCount] = $value['role'];
-                        //$_SESSION["schoolGroup" . $listCount] = $value['schoolGroup'];
-                        
-                        $listCount++;
-                    }
-                ?>
+<head>
+    <title>Admin</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <!-- VENDOR CSS -->
+    <link rel="stylesheet" href="view/vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="view/vendor/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="view/vendor/linearicons/style.css">
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="view/css/main.css">
+    <link rel="stylesheet" href="view/css/custom.css">
+    <!-- GOOGLE FONTS -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
+    <!-- ICONS -->
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicon.png">
+</head>
+<body>
+    <!-- WRAPPER -->
+    <div id="wrapper">
+        <!-- NAVBAR -->
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="brand">
+<!--                <a href="index.html"><img src="assets/img/logo-dark.png" alt="Klorofil Logo" class="img-responsive logo"></a>-->
+                <a href="/pad/view/googleAPI/logout.php">Logout from Google</a>
+            </div>
+            <div class="container-fluid">
+                <div class="navbar-btn">
+                    <button type="button" class="btn-toggle-fullwidth"><i class="lnr lnr-arrow-left-circle"></i></button>
+                </div>
+                
+                <form class="navbar-form navbar-left">
+                    <div class="input-group">
+                        <input type="text" value="" class="form-control" placeholder="Search dashboard...">
+                        <span class="input-group-btn"><button type="button" class="btn btn-primary">Go</button></span>
+                    </div>
+                </form>
+                <div id="navbar-menu">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                
+                                <img src="
+                                    <?php echo $_SESSION['userPicture'] ?>
+                                " class="img-circle" alt="Avatar"> <span><?php echo $_SESSION['userFName'] . ' ' . $_SESSION['userLName'] ?></span> <i class="icon-submenu lnr lnr-chevron-down"></i></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="/pad/view/googleAPI/logout.php"><i class="lnr lnr-exit"></i> <span>Logout</span></a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                
+            </div>
+        </nav>
+        <!-- END NAVBAR -->
+        <!-- LEFT SIDEBAR -->
+        <div id="sidebar-nav" class="sidebar">
+            <div class="sidebar-scroll">
+                <nav>
+                    <ul class="nav">
+                        <li><a href="admin.html" class="active"><i class="lnr lnr-home"></i> <span>Dashboard</span></a></li>
+                        <li><a href="#" class=""><i class="lnr lnr-cog"></i> <span>Vakken</span></a></li>
+                    </ul>
+                </nav>
+            </div>
         </div>
+        <!-- END LEFT SIDEBAR -->
+        <!-- MAIN -->
+        <div class="main">
+            <!-- MAIN CONTENT -->
+            <div class="main-content">
+                <div class="container-fluid">
+                    <h3 class="page-title">Gebruikers</h3>
+                    <?php
+                    $listCount = 0;
+                    echo '<div class="row">';
+                    foreach ($readList as $value) {
+                        if ($listCount == 6) {
+                            echo '</div>';
+                            echo '<div class="row">';
+                            $listCount = 0;
+                        }
+                        echo '<div class="col-md-2">';
+                            echo '<div class="panel">';
+                                echo '<div class="panel-body">';
+                                    echo '<a href="editUser.php?edit=' . $listCount . '">';
+                                        echo '<p name="fName ' . $listCount . '">' . $value['first_name']. ' ';
+                                        echo  $value['last_name'] . '</p>';
+                                        echo '<img class="imageUser" src="' . $value['picture'] . '">';
+                                    echo '</a>';
+                                echo '</div>';
+                            echo '</div>';    
+                        echo '</div>';
 
-        <script src="http://code.jquery.com/jquery.js"></script>
-        <script src="view/js/bootstrap.min.js"></script>
+                        $_SESSION["fName" . $listCount] = $value['first_name'];
+                        $_SESSION["lName" . $listCount] = $value['last_name'];
 
-    </body>
+                        $listCount++;
+                        
+                    }
+                    ?>
+                </div>
+            </div>
+            <!-- END MAIN CONTENT -->
+        </div>
+        <!-- END MAIN -->
+        <div class="clearfix"></div>
+    </div>
+    <!-- END WRAPPER -->
+    <!-- Javascript -->
+    <script src="view/vendor/jquery/jquery.min.js"></script>
+    <script src="view/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="view/vendor/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+    <script src="view/js/klorofil-common.js"></script>
+</body>
 </html>
