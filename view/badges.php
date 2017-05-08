9d241fe554c454af2037ec1d0c16667c6b549a03
Ron Pelt <ron.pelt@hva.nl>
2017-05-08 14:12:26 +0200

    star rating + klas toevoegen table

--- view/badges.php
+++ view/badges.php
@@ -12,6 +12,7 @@
                     // Alle gebruikers worden ingeladen
                     
                     $listCount = 0;
+                    $starCount = 0;
                     echo '<div class="row">';
                     foreach ($readList as $value) {
                         
@@ -27,7 +28,18 @@
                                     echo '<center><img class="imageUser" src="view/img/' . $value['pathToImage'] . '.png">';
                                     echo '<p>' . $value['subject_subject']. '</p> ';
                                     echo '<p>' . $value['description']. '</p></center>  ';
-                                    
+                                    echo '<hr>';
+                                    echo '<div class="starRating">';
+                                        echo '<fieldset class="rating closeRow '.$starCount.'">';
+                                            echo '<input type="radio" id="cross-'.$starCount.'" name="rating'.$starCount.'" value="3" /><label for="cross-'.$starCount.'" title="niet"><span>&#9747</span></label>';
+                                        echo '</fieldset>';
+                                        echo '<fieldset class="rating starRow '.$starCount.'">';
+                                            echo '<input type="radio" id="star3-'.$starCount.'" name="rating'.$starCount.'" value="3" /><label for="star3-'.$starCount.'" title="3"><span>&#9733</span></label>';
+                                            echo '<input type="radio" id="star2-'.$starCount.'" name="rating'.$starCount.'" value="2" /><label for="star2-'.$starCount.'" title="2"><span>&#9733</span></label>';
+                                            echo '<input type="radio" id="star1-'.$starCount.'" name="rating'.$starCount.'" value="1" /><label for="star1-'.$starCount.'" title="1"><span>&#9733</span></label>';
+                                        echo '</fieldset>';
+                                        $starCount++;
+                                    echo '</div>';
                                 echo '</div>';
                             echo '</div>';    
                         echo '</div>';
@@ -35,6 +47,7 @@
                         $listCount++;
                         
                     }
+                echo '</div>';
                     ?>
                 </div>
             </div>
